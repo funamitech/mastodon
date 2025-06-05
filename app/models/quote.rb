93a40fedc89b24f43cab7c0992664bed9442fb2c
Noa Himesaka <himesaka@noa.codes>
2025-06-06 04:24:43 +0900

    Revert "legacy quote fix, take 3"
    This reverts commit 60ff9e3521903ccdb6f91617fe252a27ae3490c0.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -40,9 +40,7 @@ class Quote < ApplicationRecord
   end
 
   def reject!
-    if legacy?
-      return
-    elsif accepted?
+    if accepted?
       update!(state: :revoked)
     elsif !revoked?
       update!(state: :rejected)
