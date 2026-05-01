e670c65171bbfb9abef8d36e62417d84eb518a7b
Noa Himesaka <himesaka@noa.codes>
2026-05-01 17:57:18 +0900

    feat: Nori tokenizer for better Korean search

--- app/chewy/public_statuses_index.rb
+++ app/chewy/public_statuses_index.rb
@@ -21,14 +21,16 @@ class PublicStatusesIndex < Chewy::Index
       },
     },
 
-    analyzer: {
-      verbatim: {
-        tokenizer: 'uax_url_email',
-        filter: %w(lowercase),
+    tokenizer: {
+      nori_user_dict: {
+        type: 'nori_tokenizer',
+        decompound_mode: 'mixed',
       },
+    },
 
+    analyzer: {
       content: {
-        tokenizer: 'standard',
+        tokenizer: 'nori_user_dict',
         filter: %w(
           lowercase
           asciifolding
@@ -60,7 +62,7 @@ class PublicStatusesIndex < Chewy::Index
   root date_detection: false do
     field(:id, type: 'long')
     field(:account_id, type: 'long')
-    field(:text, type: 'text', analyzer: 'verbatim', value: ->(status) { status.searchable_text }) { field(:stemmed, type: 'text', analyzer: 'content') }
+    field(:text, type: 'text', analyzer: 'content', value: ->(status) { status.searchable_text }) { field(:stemmed, type: 'text', analyzer: 'content') }
     field(:tags, type: 'text', analyzer: 'hashtag', value: ->(status) { status.tags.map(&:display_name) })
     field(:language, type: 'keyword')
     field(:properties, type: 'keyword', value: ->(status) { status.searchable_properties })
