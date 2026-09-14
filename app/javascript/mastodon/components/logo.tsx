b202ab96bc2dbe70080c743e8a8e2563d043de37
Noa Himesaka <himesaka@noa.codes>
2026-09-14 15:12:31 +0900

    fix: wordmark logo aspect ratio

--- app/javascript/mastodon/components/logo.tsx
+++ app/javascript/mastodon/components/logo.tsx
@@ -5,7 +5,7 @@ import classNames from 'classnames';
 import logo from '@/images/logo.svg';
 
 export const WordmarkLogo: React.FC = () => (
-  <svg viewBox='0 0 261 66' className='logo logo--wordmark' role='img'>
+  <svg viewBox='0 0 129 66' className='logo logo--wordmark' role='img'>
     <title>Mastodon</title>
     <use xlinkHref='#logo-symbol-wordmark' />
   </svg>
